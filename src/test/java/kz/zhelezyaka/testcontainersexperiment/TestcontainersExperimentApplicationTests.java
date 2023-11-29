c398d4162daeff7528b888d5459a4d7627b431c8
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-11-29 23:05:26 +0300

    Init Project

---
+++ src/test/java/kz/zhelezyaka/testcontainersexperiment/TestcontainersExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.testcontainersexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersExperimentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
